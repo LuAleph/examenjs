f84a07e59523550f9829d50598863e5f273980f4
LuAleph <cemereale@hotmail.com>
2016-05-18 22:24:06 -0500

    respondí un poco más de la pregunta 4

--- pregunta04/main.js
+++ pregunta04/main.js
@@ -7,10 +7,24 @@
 
      });
 
- $(document).ready(function(){
-	var bt_count = 1;
+/*$(document).ready(function(){
+	var bt_count = 0;
 	$("button").bind("click", function(){
 	$(this).after("<li>5" + (++bt_count) + "</li>");
+	});
+
+});*/
+
+
+ $(document).ready(function() {
+	$("button").click(function() {
+		var nuevo = parseInt($("li:last()").text());
+		var colors = $('#dos').val();
+		var algo = $('#uno').val();
+	
+		if(algo === ""){algo = "";}
+
+		$("ul:last()").append("<li></li>");	
 	});
 
 });
